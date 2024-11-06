28120a9e0ada459e7ed02f5b98572eefa1641786
Nayan Nagpal <110152854+thelazybugger@users.noreply.github.com>
2024-11-06 20:16:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 
+app.use(express.static('public')); 
+
+
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT, () => {
